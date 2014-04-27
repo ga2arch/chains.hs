f9b68be9796ab5ff16837a01210d1581f8424798
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-04-27 22:22:41 +0200

    using M.Map for dem chainz

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,17 @@
 
 module Main where 
 
+import Prelude hiding (readFile)
+
 import Control.Monad.State
 import Data.Maybe
-import Data.List (intersperse)
+import Data.List (intersperse, transpose)
 import System.Environment
+import System.IO.Strict
 
-type Chains = [Chain]
+import qualified Data.Map as M
+
+type Chains = M.Map String Chain
 
 data Chain = Chain {
     chainName  :: String
@@ -22,7 +27,7 @@ maybeRead = fmap fst . listToMaybe . reads
 process :: [String] -> StateT Chains IO ()
 process (cmd:args) |  cmd == "add"  = addChain args
                    |  cmd == "show" = showChains
-                   |  cmd == "edit" = editChains
+                   |  cmd == "done" = doneChain args
 
 editChains = undefined
 loadChains = fmap maybeRead $ readFile "chains"
@@ -31,27 +36,34 @@ addChain :: [String] -> StateT Chains IO ()
 addChain (name:_) = do
     chains <- get
     let chain = Chain name 1 0 True []
-    put $ chains ++ [chain]
+    put $ M.insert name chain chains
 
 showChains :: StateT Chains IO ()
 showChains = do
-    chains <- get
-    let r = map showChain chains
-    liftIO $ print r
+    chains <- fmap M.elems get
+    let r = transpose $ map showChain chains
+    mapM_ (\x -> liftIO $ putStrLn $ concat $ intersperse "\t" x) r
 
 showChain :: Chain -> [String]
-showChain Chain{..} = [chainName,"\n",show chainStart,"\n",progress]
+showChain Chain{..} = [chainName,show chainStart,"---",progress]
   where
     progress = concat $ 
                intersperse "\n" $ 
                map (\x -> if x then "V" else "X") chainProgress
+
+doneChain :: [String] -> StateT Chains IO ()
+doneChain (name:_) = do
+    chains <- get
+    let (Chain n s e r p) = chains M.! name
+    let nchain = Chain n s e r $ p ++ [True]
+    put $ M.insert name nchain chains
 
 main :: IO ()
 main = do
     args <- getArgs 
     chains  <- loadChains >>= \case
         Just chains -> execStateT (process args) chains 
-        Nothing     -> execStateT (process args) []
+        Nothing     -> execStateT (process args) M.empty
     writeFile "chains" $ show chains
 
 
