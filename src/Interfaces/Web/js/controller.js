3be56783309aebba6b0d1ca5f143feaca45ef251
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-05-01 22:35:01 +0200

    commit pre edit

--- src/Interfaces/Web/js/controller.js
+++ src/Interfaces/Web/js/controller.js
@@ -5,4 +5,14 @@ chainsApp.controller('chainsCtrl', function($scope, $http) {
     $http.get('chains.json').success(function(data) {
         $scope.chains = data
     })
+
+    $scope.doneChain = function(name) {
+        $scope.chains[name.toLowerCase()].progress.push(true)
+        //console.log($scope.chains)
+    }
+
+    $scope.undoChain = function (name, index) {
+        $scope.chains[name.toLowerCase()].progress[index] = false
+        console.log($scope.chains)
+    }
 })
